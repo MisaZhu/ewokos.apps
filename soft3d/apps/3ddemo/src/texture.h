dc0aebcca0922877571e33391a03ebaff362a4e5
misazhu <misa.zhu@gmail.com>
2026-08-22 06:47:18 +0800

    ewokos extra projects

---
+++ soft3d/apps/3ddemo/src/texture.h
@@ -0,0 +1,13 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <stdint.h>
+
+extern int texture_width;
+extern int texture_height;
+
+extern uint32_t* mesh_texture;
+
+void load_png_texture_data(char* filename);
+
+#endif
